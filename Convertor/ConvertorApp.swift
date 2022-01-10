969c0704336956d3250c5817f21c0e9f27c9b644
Sergey Dolgikh <ageres7@gmail.com>
2022-01-11 01:02:54 +0500

    Initial Commit

---
+++ Convertor/ConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertorApp.swift
+//  Convertor
+//
+//  Created by Sergey Dolgikh on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
